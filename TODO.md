d5ed06c62b486558129470386ecdecdc39d0c224
Bhanu Ahlawat <bhanu1322ahlawat@gmail.com>
2025-12-05 08:58:47 +0530

    responsive

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO: Make Project Responsive
+
+- [x] Update header in src/App.jsx to use responsive Tailwind classes (e.g., text sizes, padding)
+- [x] Update grid in src/index.css to be responsive (1 col mobile, 2 tablet, 3 desktop)
+- [x] Adjust container padding in src/index.css for mobile
+- [x] Run the project to test responsiveness
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NetflixSeries,{Footer} from "./components/netflixseries";import { EventHandling } from "./components/EventHandling";
 // import { EventProps } from "./components/EventProps";
-         {/* Default always out without curly bracket */}
+{/* Default always out without curly bracket */}
 // import {Footer} from "./components/netflixseries";
 // import {EventPropagation} from "./components/EventPropagation";
 import {State} from "./components/hooks/State";
@@ -10,7 +10,7 @@ import { TopHeading } from "./components/top";
 export const App=()=>{
   return(
     <>    
-    <div className="w-100%  flex items-center justify-center bg-red-800 text-white font-extrabold text-8xl">NET
+    <div className="w-full flex items-center justify-center bg-red-800 text-white font-extrabold text-4xl sm:text-6xl md:text-8xl px-4 py-2">NET
       <span className="text-black">FLIX</span>
     </div>
     <section className="container">
